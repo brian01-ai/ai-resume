101b126ec4b0e0a32e26ae4468702bade2e2de04
brian01-ai <drdinny24@gmail.com>
2025-08-15 23:28:55 +0100

    resume feedback

--- app/routes/Upload.tsx
+++ app/routes/Upload.tsx
@@ -7,6 +7,11 @@ import { convertPdfToImage } from "~/lib/pdf2Image";
 import { generateUUID } from "~/lib/utils";
 import { prepareInstructions } from "../../constants";
 
+export const meta = () => ([
+    { title: 'Resumind | Upload' },
+    { name: 'description', content: 'Upload your resume for analysis' },
+])
+
 const Upload = () => {
   const { auth, isLoading, fs, ai, kv } = usePuterStore();
   const navigate = useNavigate();
